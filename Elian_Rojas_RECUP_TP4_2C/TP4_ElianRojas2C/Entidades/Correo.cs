e0e8f100b39b7781f0a9a2773ccf1ad1f8eeb1cd
Elian <RojasElian173@gmail.com>
2019-12-08 04:39:48 -0300

    Recuperatorio tp4

---
+++ Elian_Rojas_RECUP_TP4_2C/TP4_ElianRojas2C/Entidades/Correo.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace Entidades
+{
+    public class Correo: IMostrar<List<Paquete>>
+    {
+        #region Atributos
+
+        private List<Thread> mockPaquetes;
+        private List<Paquete> paquetes;
+
+        #endregion Atributos
+
+        #region Constructores
+
+        public Correo()
+        {
+            this.mockPaquetes = new List<Thread>();
+            this.paquetes = new List<Paquete>();
+        }
+
+        #endregion Constructores
+
+        #region Propiedades
+
+        public List<Paquete> Paquetes
+        {
+            get
+            {
+                return this.paquetes;
+            }
+            set
+            {
+                this.paquetes = value;
+            }
+        }
+
+        #endregion Propiedades
+
+        #region Metodos
+
+        //interfaz
+        /// <summary>
+        /// Retorna los datos de todos los paquetes
+        /// </summary>
+        /// <param name="elementos"></param>
+        /// <returns></returns>
+        public string MostrarDatos( IMostrar<List<Paquete>> elementos )
+        {
+            string descripcion = string.Empty;
+
+            foreach (Paquete paquete in ((Correo) elementos).Paquetes)
+            {
+                descripcion += string.Format("{0} para {1} ({2}) \n", paquete.TrackingID, paquete.DireccionEntrega, paquete.Estado.ToString());
+            }
+            return descripcion;
+        }
+
+        /// <summary>
+        /// Termina con la ejecucion de todos los hilos
+        /// </summary>
+        public void FinEntregas()
+        {
+            foreach (Thread hilo in this.mockPaquetes)
+            {
+                if (hilo != null && hilo.IsAlive)
+                {
+                    hilo.Abort();
+                }
+            }
+        }
+
+        #endregion Metodos
+
+        #region Sobrecarga operadores
+
+        /// <summary>
+        /// Agrega un paquete a la lista de Correo , verificando que no sea repetido
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static Correo operator +( Correo c, Paquete p )
+        {
+            foreach (Paquete paquete in c.paquetes)
+            {
+                if (paquete == p)
+                {
+                    throw new TrackingIdRepetidoException("El paquete con ID:" + paquete.TrackingID + " ya esta cargado");
+                }
+            }
+
+            c.paquetes.Add(p);
+
+            Thread hilo = new Thread(p.MockCicloDeVida);
+
+            c.mockPaquetes.Add(hilo);
+
+            hilo.Start();
+
+            if (Paquete.threadException != null)
+            {
+                throw Paquete.threadException;
+            }
+
+            return c;
+        }
+
+        #endregion Sobrecarga operadores
+    }
+}
---
+++ Elian_Rojas_RECUP_TP4_2C/TP4_ElianRojas2C/Entidades/PaqueteDAO.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Entidades
+{
+    public static class PaqueteDAO
+    {
+        #region Atributos
+
+        private static SqlCommand comando;
+        private static SqlConnection conexion;
+
+        #endregion Atributos
+
+        #region Constructor
+
+        static PaqueteDAO()
+        {
+            string connectionString = @"Server =.\SQLEXPRESS; Database = correo-sp-2017; Trusted_Connection = true;";
+
+            conexion = new SqlConnection(connectionString);
+            comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = System.Data.CommandType.Text;
+        }
+
+        #endregion Constructor
+
+        #region Metodos
+
+        /// <summary>
+        /// Inserta un paquete en la base de datos
+        /// </summary>
+        /// <param name="paquete"></param>
+        /// <returns></returns>
+        public static bool Insertar( Paquete paquete )
+        {
+            try
+            {
+                string comandoString = "INSERT INTO dbo.Paquetes (direccionEntrega, trackingID, alumno) VALUES (@direccionEntrega, @trackingID, @alumno);";
+                PaqueteDAO.conexion.Open();
+
+                PaqueteDAO.comando = new SqlCommand(comandoString, PaqueteDAO.conexion);
+                PaqueteDAO.comando.Parameters.AddWithValue("@direccionEntrega", paquete.DireccionEntrega);
+                PaqueteDAO.comando.Parameters.AddWithValue("@trackingID", paquete.TrackingID);
+                PaqueteDAO.comando.Parameters.AddWithValue("@alumno", "Elian Rojas");
+
+                PaqueteDAO.comando.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (PaqueteDAO.conexion != null && PaqueteDAO.conexion.State == System.Data.ConnectionState.Open)
+                {
+                    PaqueteDAO.conexion.Close();
+                }
+            }
+
+            return true;
+        }
+
+        #endregion Metodos
+    }
+}
